0702b8c214d2fddfda4845e6c3813ab878f8788f
Federica d'Incalci <federicadincalci@MacBookAir.Home>
2025-09-21 10:52:56 +0200

    Added Button and Context

---
+++ src/store/CartContext.jsx
@@ -0,0 +1,71 @@
+import  { createContext, useReducer } from 'react';
+
+const CartContext = createContext({
+    items: [],
+    addItem: (item) => {},
+    removeItem: (id) => {}
+});
+
+//parameters "state" and "action" are passed automatically to the function by React when it uses it
+function cartReducer(state, action) {
+    if(action.type === 'ADD_ITEM') {
+        //check if the item already exists in the array
+        const existingCartItemIndex = state.items.findIndex(
+            item => item.id === action.item.id
+        );
+        
+        //create a new array to use to add the new item as updating the state's one is not a good practice
+        const updatedItems = [...state.items];
+
+        if(existingCartItemIndex > -1) { //already existing item
+            const existingItem = state.items[existingCartItemIndex];
+            const updatedItem = {
+                ...existingItem,
+                quantity: existingItem.quantity + 1
+            };
+            updatedItems[existingCartItemIndex] = updatedItem;
+        } else { //new item
+            updatedItems.push({
+                ...action.item, 
+                quantity: 1
+            });
+        }
+
+        return {...state, items: updatedItems};
+    }
+    if(action.type === 'REMOVE_ITEM') {
+        const existingCartItemIndex = state.items.findIndex(
+            item => item.id === action.item.id
+        );
+        const existingCartItem = state.items[existingCartItemIndex];
+
+        const updatedItems = [...state.items];
+
+        if(existingCartItem.quantity === 1) { //remove the overall item
+            updatedItems.splice(existingCartItemIndex, 1); //remove 1 item at the given index
+        } else { //reduce the quantity
+            const updatedItem = {
+                ...existingCartItem, 
+                quantity: existingCartItem.quantity - 1
+            };
+
+            updatedItems[existingCartItemIndex] = updatedItem;
+        }
+
+        return {...state, items: updatedItems};
+    }
+
+    return state;
+}
+
+export function CartContextProvider({children}) {
+    useReducer(cartReducer, { items: [] });
+    
+    return(
+        //before React 19.x you should have used 
+        //<CartContext.Provider></CartContext.Provider>
+        <CartContext>{children}</CartContext>
+    )
+}
+
+export default CartContext;
